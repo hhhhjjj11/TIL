b0f39582a4c3eda74ff1f2bcf21f015ba13f61f8
hhhhjjj11 <huhwo00@gmail.com>
2023-01-19 17:57:46 +0900

    1월 19일 목요일

--- 알고리즘/연습장.py
+++ 알고리즘/연습장.py
@@ -1,10 +1,18 @@
-N= int(input())
-li = list(map(int,input().split()))
-
-M=max(li)
-l=len(li)
-sum=0
-for i in range(l):
-    sum+=100*(li[i]/M)
-
-print(sum/l)
+T= int(input())
+for t in range(T):
+    li = list(map(int,input().split()))
+    sum=0
+    cnt=0
+    
+    for item in li:
+        sum+=item
+    sum-=li[0]
+    avg=sum/len(li)
+    
+    for i in range(1,len(li)):
+        if avg<=li[i]:
+            cnt+=1
+    total=len(li)-1
+    res=(cnt/total)*100
+    res=round(res,3)
+    print(f'{res}%')
