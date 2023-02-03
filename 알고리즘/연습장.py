909c389a3fdb2d776edfd26a5b8e41c66c376eec
HJ <huhwo00@gmail.com>
2023-02-03 17:54:51 +0900

    2월 3일 금

--- 알고리즘/연습장.py
+++ 알고리즘/연습장.py
@@ -1,26 +1,43 @@
-N = int(input())
-li1 = list(map(int,input().split()))
-M = int(input())
-li2 = list(map(int,input().split()))
+N ,M = map(int,input().split())
 
-def bin_search(n, li):
+NO =[[]]*201
+print('NO', NO)
+for _ in range(M):
+    p,q = map(int,input().split())
+    mm = min([q,p])
+    MM = max([q,p])
+    print('p,q :',p,q,'m,M :',mm,MM)
+    NO[mm].append(MM)
+    print(NO[mm])
+    
 
-    li.sort() # 5   [ -10 2 3 6 10 ]
-    mid=len(li)//2  # 2
-    leng = mid  # 2
-    while True:
-        if leng == 0 :
-            if li[mid+1]==n or li[mid-1]==n:
-                return 1
-            return 0
-        if n == li[mid]: # 10 == li[2](=3)
-            return 1
-        if n < li[mid]:
-            leng//=2
-            mid-=leng
-        else:
-            mid+=leng   # mid = mid +1 =3 
+print('NO',NO)
+res = []
 
+for i in range(1, N+1):
+    for j in range(i+1, N+1):
+        for k in range(j+1, N+1):
+            res.append({i,j,k})
 
-for item in li2:
-    print(bin_search(item, li1),end=' ')
+cnt=0
+
+for i in range(len(res)):  # 결과들 모아놓은것
+    isRight=True
+    for j, li in enumerate(NO):  # 금지조합의 인덱스와 각항
+        for v in li:
+            print('j,v',{j,v})
+            if len(res[i]-{j,v}) ==1 :
+                isRight=False    
+            if isRight==False:
+                break
+        if isRight==False:
+            break
+    if isRight==False:
+        continue
+    else:
+        cnt+=1
+        
+print(cnt)
+
+# 풀이 알아두기 . 수학문제 풀듯 푸는거 아니다. 
+# 선형계산이 최고다..
