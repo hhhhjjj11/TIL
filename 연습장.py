3a5c75daa70e96d36d650f177cf500986bf22563
허재 <huhwo00@gmail.com>
2023-02-16 23:29:54 +0900

    2월 16일 목 밤

--- 연습장.py
+++ 연습장.py
@@ -1,14 +1,30 @@
-N, L = map(int,input().split())
+T = int(input())
 
-def sol(N,L):
-    if (N-L) % L == 0:
-        return list(range((N-L)//L,(N-L)//L + L))
-    else:
-        sol(N,L+1)
+for tc in range(1,T+1):
+    res = 0
+    N = int(input())
+    li = (list(map(int,input().split())) for i in range(N))
 
+    visited = [False]*N
 
-print(sol(N,L))
+    for j in range(N):
+        
+        sum = li[0][j]
+        stack = [(0,j)]
 
+        i = 0
 
-
-
+        visited[j] = True
+        
+        while stack:
+            
+            I,J = stack.pop()
+            
+            i += 1
+            for j2 in range(N):
+                if not visited[j2]:
+                    stack.append((i,j2))
+                    visited[j2] = True
+                    break
+ 
+    print(f'#{tc} {res}')
