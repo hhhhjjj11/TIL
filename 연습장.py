10165732c17a0906f431b67bc4dc160bbcbded73
HJ <huhwo00@gmail.com>
2023-02-22 17:52:58 +0900

    2월 22일 수

--- 연습장.py
+++ 연습장.py
@@ -1,27 +1,20 @@
-# 정리
-# 미로찾기에서 해답이존재하는지 여부만 알고싶은경우는 걍 DFS가 빠를듯
-# 근데 최소경로를 찾는경우에는 bfs가 나은가 봄?
-# 왜냐하면 dfs로 찾으려면 모든 경로 다 뒤진다음 젤 짧은값 비교해야하는데
-# bfs로가면 그냥 도착하면 끝내면 되니까
+memo = [0]* 252
+memo[0] = 1
+memo[1] = 1
+memo[2] = 3
 
-from collections import deque
+def sol(X):
+    if memo[X] != 0:
+        return memo[X]
+    else:
+        memo[X] = sol(X-1) + sol(X-2)*2 
+        return memo[X]
 
-T = int(input())
+while True:
+    try:
+        pass
+        n = int(input())
+        print(sol(n))
+    except:
+        break
 
-for tc in range(1,T+1):
-    res = 0
-    N = int(input())
-
-    maze = [list(map(int,input())) for i in range(N)]
-
-    for i in range(N):  
-        for j in range(N):
-            V = maze[i][j]
-            if V == 2:
-                Si,Sj = i,j
-            elif V == 3:
-                Fi,Fj = i,j
-    
-    deck = deque()
-    
-    print(f'#{tc} {res}')
