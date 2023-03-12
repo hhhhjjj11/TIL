b734ebca61571d3e812d6310c1cddab421c4b69d
허재 <huhwo00@gmail.com>
2023-03-12 23:54:43 +0900

    3월 12일

--- 연습장4.py
+++ 연습장4.py
@@ -1,49 +1,53 @@
-T = int(input())
-for tc in range(1, T+1):
-    N, M = map(int, input().split())
+N = int(input())
 
-    flag = [list(input()) for _ in range(N)]
+initial = [list(map(int,input().split())) for _ in range(N)]
 
+cnt1 = 0
+cnt2 = 0
+cnt3 = 0
 
-    minV = 2500
-    cnt = 0
+stack=[initial]
 
-    for i1 in range(N-2):
-        cnt = 0
-        if flag[i1][0] != 'W':
-            flag[i1][0] = 'W'
-            cnt += 1
+while stack:
+    paper = stack.pop()
+    N = len(paper)
+    s=set()
+    for i in range(N):
+        for j in range(N):
+            s.add(paper[i][j])
+            if len(s)>1:
+                break   
+        if len(s)>1:
+            break
+    if len(s) ==1:
+        if paper[0][0] == 1:
+            cnt1 += 1
+        elif paper[0][0] == 0:
+            cnt2 +=1
+        elif paper[0][0] == -1:
+            cnt3 +=1
+    else: 
+        N//=3
+        if N >1:
+            for i in range(3):
+                for j in range(3):
+                    temp=[]
+                    for k in range(N):
+                        temprow=[]
+                        for l in range(N):
+                            temprow.append(paper[3*i+k][3*j+l])
+                        temp.append(temprow)
+                    stack.append(temp)
         else:
-            pass
-        for i2 in range(i1+1, N-1):
-            if flag[i2][0] != 'B':
-                flag[i2][0] = 'B'
-                cnt += 1
-            else:
-                pass
-            for i3 in range(i2+1, N):
-                if flag[i3][0] != 'R':
-                    flag[i3][0] = 'R'
-                    cnt += 1
-                else:
-                    pass
+            for i in range(3):
+                for j in range(3):
+                    if paper[i][j] == 1:
+                        cnt1 +=1
+                    elif paper[i][j] == 0:
+                        cnt2 +=1
+                    elif paper[i][j] == -1:
+                        cnt3 += 1
 
-                for j in range(1, M):  # 세로 한줄 만들었으니 가로로 넓혀감
-                    for i in range(N):
-                        if i == i1:
-                            if flag[i][j] != 'W':
-                                flag[i][j] = 'W'
-                                cnt += 1
-                        elif i == i2:
-                            if flag[i][j] != 'B':
-                                flag[i][j] = 'B'
-                                cnt += 1
-                        elif i == i3:
-                            if flag[i][j] != 'R':
-                                flag[i][j] = 'R'
-                                cnt += 1
-
-        if minV > cnt:
-            minV = cnt
-
-    print(minV)
+print(cnt3)
+print(cnt2)
+print(cnt1)
