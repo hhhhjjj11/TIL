795c565f7c9d8a9820cba872800594d724c71e16
HJ <huhwo00@gmail.com>
2023-03-13 17:53:56 +0900

    3월 13일 월요일

--- 연습장.py
+++ 연습장.py
@@ -1,13 +1,24 @@
-# 방향바꿔주는 함수
-def changedir(d,r):
-    #print('d',d,'r',r)
-    if (d =='왼'and r == 'L') or (d == '오' and r == 'D'):
-        return '아래'
-    if (d =='왼' and r =='D') or (d == '오' and r == 'L'):
-        return '위'
-    elif (d == '아래' and r == 'L') or (d== '위' and r == 'D'):
-        return  '오'
-    elif (d == '아래' and r == 'D') or (d == '위' and r =='L'):
-        return '왼'
+import sys
+input = sys.stdin.readline
 
-print(changedir('아래','D'))
+K, N  = map(int,input().split())
+
+list = []
+for _ in range(K):
+    list.append(int(input()))
+
+end = sum(list)//N
+
+start = 1
+
+while start<=end:
+    mid = (start+end)//2
+    X = 0
+    for i in range(K):
+        X += (list[i]//mid)
+    if X>=N:
+        start = mid+1
+    else:
+        end = mid -1
+
+print(end)
