354827cb7e96884a16005aabaac2b61d2efe61f3
허재 <huhwo00@gmail.com>
2023-03-21 11:48:46 +0900

    3월 21일 화요일

--- 장고실습/venv3/project3/articles/views.py
+++ 장고실습/venv3/project3/articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
-
+from .forms import ArticleForm
 # Create your views here.
 def index(req):
     articles = Article.objects.all()
@@ -11,15 +11,22 @@ def index(req):
 
 def create(req):
     if req.method == "POST":
-        title = req.POST.get('title')
-        content = req.POST.get('content')
-        article = Article.objects.create(title=title, content = content)
-        context={
-            'article': article,
-        }
-        return redirect('articles:index')
+        form = ArticleForm(req.POST)
+        if form.is_valid(): # 입력된 폼이 유효하다면
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+        return redirect('articles:create')
+        # title = req.POST.get('title')
+        # content = req.POST.get('content')
+        # article = Article.objects.create(title=title, content = content)
+        # context={
+        #     'article': article,
+        # }
+        # return redirect('articles:index')
     else:
-        return render(req, 'articles/create.html')
+        form = ArticleForm()
+        context = {'form': form }
+        return render(req, 'articles/create.html', context)
 
 
 def detail(req, pk):
@@ -39,13 +46,21 @@ def update(req, pk):
     article = Article.objects.get(pk=pk)
     # 수정요청시
     if req.method == 'POST':
-        article.title = req.POST.get('title')
-        article.content = req.POST.get('content')
-        article.save()
-        return redirect('articles:detail', pk)
+        form = ArticleForm(req.POST, instance = article)
+        if form.is_valid():
+            form.save()
+            return redirect('article:detail', pk=article.pk)
+        
+        # article.title = req.POST.get('title')
+        # article.content = req.POST.get('content')
+        # article.save()
+        #return redirect('articles:detail', pk)
     # 수정화면조회요청시
     elif req.method == 'GET':
-        context = {
-            'article': article
-        }
-        return render(req, 'articles/update.html', context)
+        form = ArticleForm(instance = article)
+
+        # context = {
+        #     'article': article
+        # }
+    context = {'form': form}
+    return render(req, 'articles/update.html', context)
