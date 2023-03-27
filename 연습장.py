bafebb0a6c1a31523042915df967606d871de45f
허재 <huhwo00@gmail.com>
2023-03-27 23:37:56 +0900

    3월 27일 밤

--- 연습장.py
+++ 연습장.py
@@ -1,7 +1,22 @@
-n = int(input())
+li = [1,2,3,4]
 
-port = list(map(int,input().split()))
+stack = [[[],[0,0,1,1]]]
+results = []
 
-L = 0
-R = n 
+print('stack',stack)
+while stack: 
+    permutation, used = stack.pop(0)
+    print(permutation, used)
+    if len(permutation) == len(li):
+        results.append(permutation)
+        continue  
+    for i in range(len(li)): 
+        if not used[i]:
+            used[i] = 1 
+            #print('used',used)
+            stack.append([permutation+[li[i]], used])
+            #print('used after',used)
+            used[i] = 0
+
+print(results)
 
--- 연습장2.py
+++ 연습장2.py
@@ -1,39 +1,12 @@
-N, L = map(int,input().split())
+li = [0]*3
 
-def f(N,L):
-    temp = 0
-    for i in range(L):
-        temp += i
-    if N - temp < L:
-        res = -1
-        return (0,res)
-    elif (N-temp) % L == 0:
-        mini = (N-temp)//L
-        res = []
-        for x in range(mini, mini+L):
-            res.append(x)
-        return (1,mini,res)
-    return(0,0)
+test = [[[],li]]
 
-if f(N,L-1)[0] == 1 and f(N,L-1)[1] == 1:
-    print(0,end=' ')
-    for x in f(N,L-1)[2]:
-        print(x,end= ' ')
-
-else:
-    while True:
-        if L > 100:
-            print(-1)
-            break
-        temp = 0
-        for i in range(L):
-            temp += i
-        if N - temp < L:
-            print(-1)
-            break
-        elif (N-temp) % L == 0:
-            mini = (N-temp)//L
-            for x in range(mini, mini+L):
-                print(x,end= ' ')
-            break
-        L += 1
+while test:
+    temp, li = test.pop()
+    print(li)
+    for i in range(2):
+        if i==1:
+            li[i] = 10
+            test.append([temp,li])
+            
