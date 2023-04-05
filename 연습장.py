b85ada9cf2d3490798121d15f0d4fc465c0d809c
허재 <huhwo00@gmail.com>
2023-04-05 22:38:00 +0900

    4월 5일 밤

--- 연습장.py
+++ 연습장.py
@@ -2,15 +2,22 @@ from collections import deque
 inf = float('inf')
 def solve():
     n = int(input())
+    # 확률..
     prob = [list(map(int, input().split())) for _ in range(n)]
+    # 2^n개 -1생성
+    
     dp = [-1 for _ in range(1 << n)]
+    # 첫항은 1로
     dp[0] = 1
     que = deque([0])
     for i in range(n):
         for _ in range(len(que)):
+            # 덱에서 꺼낸거를 상태
             status = que.popleft()
-            for j in range(n):
+            for j in range(n): # J : 0~ n-1
+                # 상태랑 비트마스킹이 겹치는게없으면
                 if not status & (1 << j):
+                    # dp는 -1
                     if dp[status | (1 << j)] == -1:
                         que.append(status | (1 << j))
                     dp[status | (1 << j)] = max(dp[status] * prob[i][j], dp[status | (1 << j)])
