4d2aa034fae761daa68b588f3cfbc99525523894
허재 <huhwo00@gmail.com>
2023-04-08 22:50:13 +0900

    4월 8일 밤

--- 연습장2.py
+++ 연습장2.py
@@ -1,32 +1,12 @@
-from collections import deque
-
 N = int(input())
 
-g = [list(map(int,input().split())) for _ in range(N)]
+adj_matrix = [list(map(int,input().split())) for _ in range(N)]
 
-# 노드 : 0 ~ N-1 번
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if adj_matrix[i][k] == 1 and adj_matrix[k][j] == 1:
+                adj_matrix[i][j] = 1
 
-Ans = [[0]*N for _ in range(N)]
-
-# bfs
-visited = []
-for i in range(N):
-    for j in range(N):
-        if i not in visited:
-            deck = deque()
-            deck.append(i)
-
-            while deck:
-                node_now = deck.popleft()
-                
-                for node_next in range(N):
-                    if node_now == node_next:
-                        continue
-                    if g[node_now][node_next]: # 길이 있으면
-                        pass
-
-        visited.append(i)
-
-
-# 아이디어
-# 
+for row in adj_matrix:
+    print(' '.join(map(str,row)))
