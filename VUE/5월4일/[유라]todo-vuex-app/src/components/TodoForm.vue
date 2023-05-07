33ba882594686fa8396f724fd3078f5610d872f2
HJ <huhwo00@gmail.com>
2023-05-08 08:35:29 +0900

    5월8일

---
+++ VUE/5월4일/[유라]todo-vuex-app/src/components/TodoForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- trim :양쪽 공백 없애줌 -->
+    <input type="text" v-model.trim="todoTitle" @keyup.enter="createTodo">
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'TodoForm',
+    data(){
+        return {
+            todoTitle: null,
+        }
+    },
+    methods: {
+        createTodo(){
+            if(this.todoTitle){
+                this.$store.dispatch('createTodo', this.todoTitle)
+            } else{
+                alert('할일을 입력하세요!!')
+            }
+            this.todoTitle = null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/5월4일/[유라]todo-vuex-app/src/components/TodoListItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>Todo.. example</p>
+        <p>{{ todoitem.title }}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoListItem',
+    props:{
+        todoitem: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
