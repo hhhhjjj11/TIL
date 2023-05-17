a092881fe3688226d3869c829812486db0757907
HJ <huhwo00@gmail.com>
2023-05-17 18:01:10 +0900

    5월 17일 수요일

--- 장고실습/venv11_4월17일/01_drf_template/articles/serializers.py
+++ 장고실습/venv11_4월17일/01_drf_template/articles/serializers.py
@@ -2,9 +2,6 @@ from rest_framework import serializers
 from .models import Article, Comment
 
 
-
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
